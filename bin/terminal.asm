3d98d97c81eaadac16845c51ed3bb1b4052203d0
Seos740 <dicksonstanley503@gmail.com>
2025-04-02 07:23:29 +0800

    Added Keypress Support, Added Procces Configs
    Added Keypress Support:
    Implemented functionality to capture and process user keypresses, allowing the program to respond to keyboard input in real-time. Key events, such as Enter and Backspace, are now handled, enabling dynamic interaction with the system.
    
    Added Process Configs:
    Developers can now define the memory requirements and privilege levels for their applications. This allows better resource management and control, ensuring that each app has the necessary resources and permissions for efficient execution.

--- bin/terminal.asm
+++ bin/terminal.asm
@@ -1,9 +1,10 @@
 [bits 32]
-mov al, 0x03
-    out 0x03C8, al
 
-    mov edi, 0xB8000
-    mov esi, msg
+mov al, 0x03
+out 0x03C8, al
+
+mov edi, 0xB8000
+mov esi, msg
 
 print_char:
     mov al, [esi]
@@ -17,5 +18,26 @@ print_char:
 
 msg:
     db 'Welcome to StOS!', 0x0d, 0x0a, 'StOS $ ', 0
+
 newcommand:
     db 0x0d, 0x0a, 'StOS $ ', 0
+
+read_keyboard:
+    cmp eax, 0x1c
+    je print_new_command
+    mov ah, 0x0F      
+    mov word [edi], ax
+    add edi, 2        
+    iret
+
+print_new_command:
+    mov esi, newcommand
+new_command_loop:
+    mov al, [esi]
+    cmp al, 0
+    je read_keyboard
+    mov ah, 0x0F
+    mov word [edi], ax
+    add edi, 2
+    inc esi
+    jmp new_command_loop
