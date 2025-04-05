353e4a45abc20277a56c3c740ca4c6ce9dd18e21
Seos740 <dicksonstanley503@gmail.com>
2025-04-05 20:45:20 +0800

    Update example.exs

--- opt/example.exs
+++ opt/example.exs
@@ -1,4 +1,5 @@
-header data------------data------------data------------data------------data-----
+header--
+data------------data------------data------------data------------data-----
 code----etc----code
 data----etc----data
 0x0000 (Null Descriptor)
