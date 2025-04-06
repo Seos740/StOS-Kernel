1516d34e2f08e7c26178297a8ec67c1da101efb3
Seos740 <dicksonstanley503@gmail.com>
2025-04-07 07:39:14 +0800

    Updated the kernel_entry

---
+++ EFI/StOS/kernel_entry.c
@@ -0,0 +1,13 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_FILE_IO_INTERFACE *FileIO;
+EFI_FILE_HANDLE Volume;
+EFI_FILE_HANDLE File;
+
+Print(L"Hello, StOS!"\n);
+Print(L"Beta Num: %d\n", 1);
+Print(L"StOS $ ");
+
+
+
